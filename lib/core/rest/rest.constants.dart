6ed75b7de28474a3187ea2822a2c41e4f4d35a11
JJSosaL <hello@fancystudio.xyz>
2026-04-19 22:06:33 +0200

    Usar comillas simples

--- lib/core/rest/rest.constants.dart
+++ lib/core/rest/rest.constants.dart
@@ -1,3 +1,3 @@
 final String baseUriDomain = String.fromEnvironment('API_DOMAIN', defaultValue: 'localhost:3001');
 
-final bool useHttps = bool.fromEnvironment("USE_HTTPS", defaultValue: false);
+final bool useHttps = bool.fromEnvironment('USE_HTTPS', defaultValue: false);
