d1216616ffadac0808ff7f9a0b40f5e8f6e67965
JJSosaL <hello@fancystudio.xyz>
2026-04-19 22:04:34 +0200

    Áñadir variable `useHttps`

--- lib/core/rest/rest.constants.dart
+++ lib/core/rest/rest.constants.dart
@@ -1,1 +1,3 @@
-final String baseUriDomain = String.fromEnvironment('API_DOMAIN');
+final String baseUriDomain = String.fromEnvironment('API_DOMAIN', defaultValue: 'localhost:3001');
+
+final bool useHttps = bool.fromEnvironment("USE_HTTPS", defaultValue: false);
