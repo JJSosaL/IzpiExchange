c07c683b134151ae13283322970a042014989622
JJSosaL <hello@fancystudio.xyz>
2026-04-20 23:59:53 +0200

    Cambiar página por defecto

--- lib/features/home/home.screen.dart
+++ lib/features/home/home.screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:izpi_exchange/shared/widgets/button.dart';
+import 'package:izpi_exchange/shared/styles/text.font.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,54 +7,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            spacing: 10,
-            children: [const HomeTitle(), const HomeSignInButton(), const HomeSignUpButton()],
+          padding: EdgeInsetsGeometry.all(30),
+          child: Text(
+            '¡Bienvenido!',
+            style: defaultFont(fontSize: 30, fontWeight: FontWeight.bold),
           ),
         ),
       ),
-    );
-  }
-}
-
-class HomeTitle extends StatelessWidget {
-  const HomeTitle({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'Bienvenido',
-      style: GoogleFonts.bricolageGrotesque(fontSize: 30, fontWeight: FontWeight.bold),
-    );
-  }
-}
-
-class HomeSignInButton extends StatelessWidget {
-  const HomeSignInButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ButtonWidget(
-      content: 'Iniciar Sesión',
-      onPressed: () => context.push('/sign-in'),
-      variant: ButtonWidgetVariant.outline,
-    );
-  }
-}
-
-class HomeSignUpButton extends StatelessWidget {
-  const HomeSignUpButton({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ButtonWidget(
-      content: 'Crear Cuenta',
-      onPressed: () => context.push('/sign-up'),
-      variant: ButtonWidgetVariant.filled,
     );
   }
 }
