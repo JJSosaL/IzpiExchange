54e5864a89575e65b42cc1acedd54a87275d61fc
JJSosaL <hello@fancystudio.xyz>
2026-04-20 23:55:38 +0200

    Extraer y verificar si es un string vacío

--- lib/core/rest/rest.constants.dart
+++ lib/core/rest/rest.constants.dart
@@ -1,3 +1,4 @@
-final String baseUriDomain = String.fromEnvironment('API_DOMAIN', defaultValue: 'localhost:3001');
+final String baseUriDomainFromEnv = String.fromEnvironment('API_DOMAIN');
+final String baseUriDomain = baseUriDomainFromEnv.isEmpty ? 'localhost:3001' : baseUriDomainFromEnv;
 
-final bool useHttps = bool.fromEnvironment('USE_HTTPS', defaultValue: false);
+final bool useHttps = bool.fromEnvironment('USE_HTTPS');
